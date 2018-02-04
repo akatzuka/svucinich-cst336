54f6c63d8034721d8039579a4563ce902391b6ec
Sean <svucinich@csumb.edu>
2018-02-04 04:21:09 +0000

    lab 1 done

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<a href="./svucinich-cst336/Lab_1/index.html"> Lab 1 </a>
+<body>
+    <ul>
+        <li>
+        <a href="./svucinich-cst336/Lab_1/index.html">Lab 1 </a>
+        </li>
+    </ul>
+</body>
