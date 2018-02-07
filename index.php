b468eaf76ba5ad51e621d6f456bd878449a6aebb
Sean <svucinich@csumb.edu>
2018-02-07 21:21:46 +0000

    HW1 Done

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
     <ul>
         <li>
             <a href="./Labs/Lab_1/index.html">Lab 1 </a>
+        </li>
+        <li>
             <a href="./Labs/Lab_2/index.php">Lab 2 </a>
         </li>
+        <li>
+            <a href="./HW/HW_1/index.html">HW 1 </a>
+        </li>          
     </ul>
 </body>
