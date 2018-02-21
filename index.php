0974790e3d7a01c215a9c4d9fe4f0970dea8122d
Sean <svucinich@csumb.edu>
2018-02-21 11:45:59 +0000

    Homework and Challenges added to Index

--- index.php
+++ index.php
@@ -7,7 +7,22 @@
             <a href="./Labs/Lab_2/index.php">Lab 2 </a>
         </li>
         <li>
+            <a href="./Labs/Lab_2/index.php">Lab 4 </a>
+        </li>
+        <li>
             <a href="./HW/HW_1/index.html">HW 1 </a>
-        </li>          
+        </li>    
+        <li>
+            <a href="./HW/HW_2/index.php">HW 2 </a>
+        </li>
+        <li>
+            <a href="./Challenges/C_1/season.html">Challenge 1 </a>
+        </li>    
+        <li>
+            <a href="./Challenges/C_2/index.php">Challenge 2 </a>
+        </li> 
+        <li>
+            <a href="./Challenges/C_3/index.php">Challenge 3 </a>
+        </li> 
     </ul>
 </body>
