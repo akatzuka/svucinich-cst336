c825671d5548236332372ea4f9a2956abe6597f2
Sean <svucinich@csumb.edu>
2018-04-01 05:46:44 +0000

    Lab 6 Done?

--- Labs/Lab_6/index.php
+++ Labs/Lab_6/index.php
@@ -74,6 +74,10 @@
 <html>
     <head>
         <title> OtterMart Product Search </title>
+    <style>
+        @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+        @import url("css/styles.css");
+    </style>
     </head>
     <body>
 
