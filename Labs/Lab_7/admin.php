8e05958ca0e8e227c9af0ca1bed196ced5454667
Sean <svucinich@csumb.edu>
2018-04-15 04:25:37 +0000

    Lab 7 Done

--- Labs/Lab_7/admin.php
+++ Labs/Lab_7/admin.php
@@ -46,7 +46,10 @@ function displayAllProducts(){
     </head>
     <body>
 
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
         
         <h1> Admin Main Page </h1>
         
